2c53004592543e3df990f626856d92d3323eb391
Alejo Arias <jalejandro0211@hotmail.com>
2022-05-21 12:07:19 -0500

    version sabado

--- components/GraphScreen.js
+++ components/GraphScreen.js
@@ -1,13 +1,68 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
 import NavigationMenu from './tools/NavigationMenu';
+import { LineChart } from 'react-native-chart-kit';
 
 export default function GraphScreen({ navigation }) {
+	const [list_dots, setlist_dots] = useState(null);
+	const get_dots = () => {
+		fetch('https://kyxyx7.deta.dev/get_dots_app/1/', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then((response) => {
+			if (response.status === 200) {
+				return response.json().then((json_response) => {
+					setlist_dots(json_response.data);
+					return false;
+				});
+			} else {
+				Alert.alert('fallo');
+			}
+		});
+	};
+	useEffect(() => {
+		get_dots();
+	}, []);
 	return (
 		<View style={styles.container}>
-			<NavigationMenu />
-			<Pressable onPress={() => navigation.navigate('home')}>
-				<Text>Graficas</Text>
-			</Pressable>
+			<NavigationMenu></NavigationMenu>
+			<LineChart
+				data={{
+					labels: list_dots?.list_1_dots ? list_dots.list_1_dots : [0],
+					datasets: [
+						{
+							data: list_dots?.list_1_data ? list_dots.list_1_data : [0],
+						},
+					],
+				}}
+				width={400} // from react-native
+				height={220}
+				yAxisLabel=''
+				yAxisInterval={1} // optional, defaults to 1
+				chartConfig={{
+					backgroundColor: '#e26a00',
+					backgroundGradientFrom: '#fb8c00',
+					backgroundGradientTo: '#ffa726',
+					decimalPlaces: 2, // optional, defaults to 2dp
+					color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+					labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+					style: {
+						borderRadius: 16,
+					},
+					propsForDots: {
+						r: '6',
+						strokeWidth: '2',
+						stroke: '#ffa726',
+					},
+				}}
+				bezier
+				style={{
+					marginVertical: 8,
+					borderRadius: 16,
+				}}
+			></LineChart>
 		</View>
 	);
 }
--- components/MachinesScreen.js
+++ components/MachinesScreen.js
@@ -1,13 +1,42 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Pressable, Alert } from 'react-native';
 import NavigationMenu from './tools/NavigationMenu';
 
 export default function MachinesScreen({ navigation }) {
+	const [list_machine, setlist_machine] = useState([]);
+	const get_machine = () => {
+		fetch('https://kyxyx7.deta.dev/create_machine/', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then((response) => {
+			if (response.status === 200) {
+				return response.json().then((json_response) => {
+					setlist_machine(json_response['result']);
+					return false;
+				});
+			} else {
+				Alert.alert('fallo');
+			}
+		});
+	};
+	useEffect(() => {
+		get_machine();
+	}, []);
+
 	return (
 		<View style={styles.container}>
 			<NavigationMenu />
-			<Pressable onPress={() => navigation.navigate('home')}>
-				<Text>Maquina</Text>
-			</Pressable>
+			<View style={styles.container_machines}>
+				{list_machine.map((item, index) => {
+					return (
+						<Text style={styles.text_machines} key={index}>
+							- {item.machine_name}
+						</Text>
+					);
+				})}
+			</View>
 		</View>
 	);
 }
@@ -18,5 +47,21 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+
+	container_machines: {
+		width: 300,
+		height: 'auto',
+		backgroundColor: 'silver',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 20,
+	},
+
+	text_machines: {
+		fontSize: 25,
+		color: 'white',
 	},
 });
