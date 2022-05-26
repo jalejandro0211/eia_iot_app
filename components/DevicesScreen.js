69e83db3c437a4b14758aa0d1f6b3b51d297c44a
Alejo Arias <jalejandro0211@hotmail.com>
2022-05-26 18:35:02 -0500

    formulario parte1

--- components/DevicesScreen.js
+++ components/DevicesScreen.js
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Pressable,
+	KeyboardAvoidingView,
+	Keyboard,
+	TextInput,
+} from 'react-native';
 import NavigationMenu from './tools/NavigationMenu';
 
 export default function DevicesScreen({ navigation }) {
 	return (
-		<View style={styles.container}>
-			<NavigationMenu />
-			<Pressable onPress={() => navigation.navigate('home')}>
-				<Text>Dispositivos</Text>
-			</Pressable>
-		</View>
+		<KeyboardAvoidingView style={styles.container}>
+			<NavigationMenu></NavigationMenu>
+			<View style={styles.container_form}>
+				<Text style={styles.title_form}>Dispositivo</Text>
+				<View style={styles.row_form}>
+					<Text style={styles.title_form}>Nombre:</Text>
+					<TextInput style={styles.input_form}></TextInput>
+				</View>
+			</View>
+		</KeyboardAvoidingView>
 	);
 }
 
@@ -18,5 +31,33 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	container_form: {
+		width: 300,
+		height: '60%',
+		backgroundColor: 'gray',
+		borderRadius: 20,
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	row_form: {
+		width: '95%',
+		height: 30,
+		// backgroundColor: 'red',
+		marginTop: 40,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+	},
+	title_form: {
+		fontSize: 20,
+		color: 'white',
+	},
+	input_form: {
+		width: 150,
+		height: '100%',
+		backgroundColor: 'white',
+		borderRadius: 5,
 	},
 });
